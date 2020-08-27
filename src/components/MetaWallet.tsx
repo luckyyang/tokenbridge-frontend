68c2b80a9777e92ab1ec3de22429e15263ecd73e
Harry Liu <2411mail@gmail.com>
2020-08-27 16:08:56 +0800

    add bridge contracts

--- src/components/MetaWallet.tsx
+++ src/components/MetaWallet.tsx
@@ -1,8 +1,18 @@
 import * as React from "react";
 import * as TruffleContract from "truffle-contract";
-import * as Web3 from "web3";
+// import * as Web3 from "web3";
+import Web3 = require('web3');
+import BN = require("bn.js");
+import { DAI_TOKEN, ContractsInfo } from '../util/constants';
 
-const MainTokenContract = TruffleContract(require("../../build/contracts/MainToken.json"));
+console.log(DAI_TOKEN);
+
+const MainTokenContract = TruffleContract(
+  require("../../build/contracts/MainToken.json")
+);
+const BridgeContract = TruffleContract(
+  require("../../build/contracts/Bridge_v1.json")
+);
 // import IMainToken from "../contract-interfaces/IMainToken";
 
 interface IMetaWalletProps {
@@ -12,68 +22,100 @@ interface IMetaWalletProps {
 interface IMetaWalletState {
   account: string;
   accountError: boolean;
-  totalSupply: string;
+  balance: string;
   contractAddress: string;
 }
 
-export default class MetaWallet extends React.Component<IMetaWalletProps, IMetaWalletState> {
+export default class MetaWallet extends React.Component<
+  IMetaWalletProps,
+  IMetaWalletState
+> {
   constructor(props) {
     super(props);
     this.state = {
       account: "",
       accountError: false,
-      totalSupply: "",
+      balance: "",
       contractAddress: "",
     };
   }
 
   public async componentWillMount() {
-    if (this.props.web3.eth.accounts.length === 0) {
+    const { web3 } = this.props;
+    const accounts = await (window as any).ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = accounts[0];
+
+    if (!account) {
       this.setState({
         account: "",
         accountError: true,
       });
       return;
     }
-    MainTokenContract.setProvider(this.props.web3.currentProvider);
-    // let instance: IMainToken;
-    let instance;
+    MainTokenContract.setProvider(web3.currentProvider);
+    BridgeContract.setProvider(web3.currentProvider);
+    // let tokenContract: IMainToken;
+    let tokenContract;
+    let bridgeContract;
     try {
-      // instance = await MainTokenContract.deployed();
-      instance = await MainTokenContract.at("0x2f1F4609180ceDb2cEB63c80cE6eCF94F4C0c394");
-      console.log('instance: ', instance)
+      tokenContract = await MainTokenContract.at(DAI_TOKEN[42].address);
+      bridgeContract = await BridgeContract.at(ContractsInfo[42].bridge);
+
+      console.log("tokenContract: ", tokenContract);
+      console.log("bridgeContract: ", bridgeContract);
     } catch (err) {
       alert(err);
       return;
     }
-    const accounts = await (window as any).ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
-    // const balance  = await instance.getBalance(this.props.web3.eth.accounts[0]);
-    let totalSupply;
+
+    // const balance  = await tokenContract.getBalance(web3.eth.accounts[0]);
+    let balance;
     try {
-      totalSupply = await instance.name();
-      console.log('totalSupply: ', totalSupply)
+      balance = await tokenContract.getBalance(account);
+      console.log("balance: ", balance);
     } catch (error) {
-      console.log('error when get totalSupply: ', error)
+      console.log("error when get balance: ", error);
+    }
+    try {
+      console.log("tokenContract.address: ", tokenContract.address);
+
+      const withdraw = new BN(web3.fromWei(10**18, "ether"));
+      console.log('withdraw: ', withdraw.toString())
+      const result = await tokenContract.approve(bridgeContract.address, withdraw.toString(), { from: account });
+      // await tokenContract.send({from: account, gas:70_000}, async (err, txHash) => {
+      //   try {
+      //       let receipt = await waitForReceipt(txHash);
+
+      //   } catch(err) {
+      //       console.log('Execution failed: ', err);
+      //   }
+      // });
+      console.log("result: ", result);
+    } catch (error) {
+      console.log("error happened: ", error);
     }
     this.setState({
-      // account: this.props.web3.eth.accounts[0],
+      // account: web3.eth.accounts[0],
       account,
       accountError: false,
-      // balance: balance.toString(),
-      contractAddress: instance.address,
-      totalSupply,
+      balance: balance.toString(),
+      contractAddress: tokenContract.address,
     });
   }
 
   public render() {
     return (
-    <div>
-      <h3>MainToken</h3>
-      <p>Contract address: {this.state.contractAddress}</p>
-      <p>Account: {this.state.accountError ? "No accounts found" : this.state.account}</p>
-      <p>totalSupply: {this.state.totalSupply}</p>
-    </div>
+      <div>
+        <h3>MainToken</h3>
+        <p>Contract address: {this.state.contractAddress}</p>
+        <p>
+          Account:{" "}
+          {this.state.accountError ? "No accounts found" : this.state.account}
+        </p>
+        <p>balance: {this.state.balance}</p>
+      </div>
     );
   }
 }

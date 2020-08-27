355eed4f09a39a4358ed153e01e23b5beedd49aa
Harry Liu <2411mail@gmail.com>
2020-08-27 15:58:49 +0800

    add constants

---
+++ src/util/constants.ts
@@ -0,0 +1,35 @@
+export const DAI_TOKEN = {
+  token: "DAI",
+  name: "Dai Stablecoin",
+  icon:
+    "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png",
+  30: {
+    symbol: "rDAI",
+    address: "0x6b1a73d547f4009a26b8485b63d7015d248ad406",
+    decimals: 18,
+  },
+  31: {
+    symbol: "rKovDAI",
+    address: "0x0d86fca9be034a363cf12c9834af08d54a10451c",
+    decimals: 18,
+  },
+  1: {
+    symbol: "DAI",
+    address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    decimals: 18,
+  },
+  42: {
+    symbol: "DAI",
+    address: "0xc7cc3413f169a027dccfeffe5208ca4f38ef0c40",
+    decimals: 18,
+  },
+};
+
+export const ContractsInfo = {
+  42: {
+    bridge: "0x2f1F4609180ceDb2cEB63c80cE6eCF94F4C0c394",
+    federation: "0x41c16473b12211892C813F52815F700440471AA0",
+    testToken: "0x41c16473b12211892c813f52815f700440471aa0",
+    allowTokens: "0x5D1fE6BD8C57bEA32903079759e89900A462Ddc2",
+  },
+}
